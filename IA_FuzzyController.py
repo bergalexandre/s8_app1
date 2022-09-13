fefec4a330032794a0a6a55236aaf0dff5375b74
Alexandre Bergeron <bera2920@usherbrooke.ca>
2022-09-13 16:28:41 -0400

    antecendent pour x et y commun (inachevé)

--- IA_FuzzyController.py
+++ IA_FuzzyController.py
@@ -11,65 +11,77 @@ class IA_FuzzyController_left_right:
         #Le linspace contient des distances
         perception_distance = PERCEPTION_RADIUS*maze_tile_size
 
-        wall_antecedent = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'wall_x')
-        wall_antecedent = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'wall_y')
-        obstacle_antecedent = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'obstacle')
-        greedy_antecedent = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'greedy')
+        wall_antecedent_x = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'wall_x')
+        wall_antecedent_y = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'wall_y')
+
+        obstacle_antecedent_x = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'obstacle_x')
+        obstacle_antecedent_y = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'obstacle_y')
+
+        greedy_antecedent_x = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'greedy_x')
+        greedy_antecedent_y = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'greedy_y')
+
         movement_x_consequence = ctrl.Consequent(np.linspace(-1, 1, 1000), 'movement', defuzzify_method='centroid')
         movement_y_consequence = ctrl.Consequent(np.linspace(-1, 1, 1000), 'movement', defuzzify_method='centroid')
 
         #membership
         loin_gauche = [-perception_distance, -perception_distance, -perception_distance*0.3, -perception_distance*0.15]
         proche_gauche = [-perception_distance*0.3, -perception_distance*0.15, 0, 0]
-        wall_antecedent["loin_gauche"] = fuzz.trapmf(wall_antecedent.universe, loin_gauche)
-        wall_antecedent["proche_gauche"] = fuzz.trapmf(wall_antecedent.universe, proche_gauche)
-        wall_antecedent["proche_droite"] = fuzz.trapmf(wall_antecedent.universe, np.flip(np.abs(proche_gauche)))
-        wall_antecedent["loin_droite"] = fuzz.trapmf(wall_antecedent.universe, np.flip(np.abs(loin_gauche)))
+        wall_antecedent_x["loin_gauche"] = fuzz.trapmf(wall_antecedent_x.universe, loin_gauche)
+        wall_antecedent_x["proche_gauche"] = fuzz.trapmf(wall_antecedent_x.universe, proche_gauche)
+        wall_antecedent_x["proche_droite"] = fuzz.trapmf(wall_antecedent_x.universe, np.flip(np.abs(proche_gauche)))
+        wall_antecedent_x["loin_droite"] = fuzz.trapmf(wall_antecedent_x.universe, np.flip(np.abs(loin_gauche)))
 
-        obstacle_antecedent["loin_gauche"] = fuzz.trapmf(obstacle_antecedent.universe, loin_gauche)
-        obstacle_antecedent["proche_gauche"] = fuzz.trapmf(obstacle_antecedent.universe, proche_gauche)
-        obstacle_antecedent["proche_droite"] = fuzz.trapmf(obstacle_antecedent.universe, np.flip(np.abs(proche_gauche)))
-        obstacle_antecedent["loin_droite"] = fuzz.trapmf(obstacle_antecedent.universe, np.flip(np.abs(loin_gauche)))
+        obstacle_antecedent_x["loin_gauche"] = fuzz.trapmf(obstacle_antecedent_x.universe, loin_gauche)
+        obstacle_antecedent_x["proche_gauche"] = fuzz.trapmf(obstacle_antecedent_x.universe, proche_gauche)
+        obstacle_antecedent_x["proche_droite"] = fuzz.trapmf(obstacle_antecedent_x.universe, np.flip(np.abs(proche_gauche)))
+        obstacle_antecedent_x["loin_droite"] = fuzz.trapmf(obstacle_antecedent_x.universe, np.flip(np.abs(loin_gauche)))
 
-        wall_antecedent["loin_bas"] = fuzz.trapmf(wall_antecedent.universe, loin_gauche)
-        wall_antecedent["proche_bas"] = fuzz.trapmf(wall_antecedent.universe, proche_gauche)
-        wall_antecedent["proche_haut"] = fuzz.trapmf(wall_antecedent.universe, np.flip(np.abs(proche_gauche)))
-        wall_antecedent["loin_haut"] = fuzz.trapmf(wall_antecedent.universe, np.flip(np.abs(loin_gauche)))
+        wall_antecedent_y["loin_bas"] = fuzz.trapmf(wall_antecedent_y.universe, loin_gauche)
+        wall_antecedent_y["proche_bas"] = fuzz.trapmf(wall_antecedent_y.universe, proche_gauche)
+        wall_antecedent_y["proche_haut"] = fuzz.trapmf(wall_antecedent_y.universe, np.flip(np.abs(proche_gauche)))
+        wall_antecedent_y["loin_haut"] = fuzz.trapmf(wall_antecedent_y.universe, np.flip(np.abs(loin_gauche)))
 
-        obstacle_antecedent["loin_bas"] = fuzz.trapmf(obstacle_antecedent.universe, loin_gauche)
-        obstacle_antecedent["proche_bas"] = fuzz.trapmf(obstacle_antecedent.universe, proche_gauche)
-        obstacle_antecedent["proche_haut"] = fuzz.trapmf(obstacle_antecedent.universe, np.flip(np.abs(proche_gauche)))
-        obstacle_antecedent["loin_haut"] = fuzz.trapmf(obstacle_antecedent.universe, np.flip(np.abs(loin_gauche)))
-
-        movement_consequence['bas'] = fuzz.trapmf(movement_consequence.universe, [-1, -1, -0.3 , 0])
-        movement_consequence['milieu'] = fuzz.trimf(movement_consequence.universe, [-0.3,0,0.3])
-        movement_consequence['haut'] = fuzz.trapmf(movement_consequence.universe, [0, 0.3, 1, 1])
+        obstacle_antecedent_y["loin_bas"] = fuzz.trapmf(obstacle_antecedent_y.universe, loin_gauche)
+        obstacle_antecedent_y["proche_bas"] = fuzz.trapmf(obstacle_antecedent_y.universe, proche_gauche)
+        obstacle_antecedent_y["proche_haut"] = fuzz.trapmf(obstacle_antecedent_y.universe, np.flip(np.abs(proche_gauche)))
+        obstacle_antecedent_y["loin_haut"] = fuzz.trapmf(obstacle_antecedent_y.universe, np.flip(np.abs(loin_gauche)))
 
         movement_x_consequence['gauche'] = fuzz.trapmf(movement_x_consequence.universe, [-1, -1, -0.5 , -0.0])
         movement_x_consequence['milieu'] = fuzz.trimf(movement_x_consequence.universe, [-0.3,0,0.3])
         movement_x_consequence['droite'] = fuzz.trapmf(movement_x_consequence.universe, [0.0, 0.5, 1, 1])
 
+        movement_y_consequence['bas'] = fuzz.trapmf(movement_y_consequence.universe, [-1, -1, -0.3 , 0])
+        movement_y_consequence['milieu'] = fuzz.trimf(movement_y_consequence.universe, [-0.3,0,0.3])
+        movement_y_consequence['haut'] = fuzz.trapmf(movement_y_consequence.universe, [0, 0.3, 1, 1])
+
         #rules
         rules = []
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_gauche'] & obstacle_antecedent['loin_gauche']), consequent=movement_consequence['milieu']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_gauche'] & obstacle_antecedent['loin_gauche']), consequent=movement_consequence['droite']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_droite'] & obstacle_antecedent['loin_gauche']), consequent=movement_consequence['gauche']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_droite'] & obstacle_antecedent['loin_gauche']), consequent=movement_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_x['loin_gauche'], consequent=movement_x_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_x['proche_gauche'], consequent=movement_x_consequence['droite']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_x['proche_droite'], consequent=movement_x_consequence['gauche']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_x['loin_droite'], consequent=movement_x_consequence['milieu']))
 
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_gauche'] & obstacle_antecedent['proche_gauche']), consequent=movement_consequence['droite']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_gauche'] & obstacle_antecedent['proche_gauche']), consequent=movement_consequence['droite']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_droite'] & obstacle_antecedent['proche_gauche']), consequent=movement_consequence['milieu']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_droite'] & obstacle_antecedent['proche_gauche']), consequent=movement_consequence['gauche']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_y['loin_bas'], consequent=movement_y_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_y['proche_bas'], consequent=movement_y_consequence['droite']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_y['proche_haut'], consequent=movement_y_consequence['gauche']))
+        rules.append(ctrl.Rule(antecedent=wall_antecedent_y['loin_haut'], consequent=movement_y_consequence['milieu']))
 
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_gauche'] & obstacle_antecedent['proche_droite']), consequent=movement_consequence['gauche']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_gauche'] & obstacle_antecedent['proche_droite']), consequent=movement_consequence['milieu']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_droite'] & obstacle_antecedent['proche_droite']), consequent=movement_consequence['gauche']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_droite'] & obstacle_antecedent['proche_droite']), consequent=movement_consequence['gauche']))
+        rules.append(ctrl.Rule(antecedent=obstacle_antecedent_x['loin_gauche'], consequent=movement_x_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_gauche'], obstacle_antecedent_y['proche_bas']), consequent=movement_x_consequence['droite']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_gauche'], obstacle_antecedent_y['proche_haut']), consequent=movement_x_consequence['droite']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_gauche'], obstacle_antecedent_y['loin_bas']), consequent=movement_x_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_gauche'], obstacle_antecedent_y['loin_haut']), consequent=movement_x_consequence['milieu']))
+        
+        rules.append(ctrl.Rule(antecedent=obstacle_antecedent_x['loin_droite'], consequent=movement_x_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_droite'], obstacle_antecedent_y['proche_bas']), consequent=movement_x_consequence['gauche']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_droite'], obstacle_antecedent_y['proche_haut']), consequent=movement_x_consequence['gauche']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_droite'], obstacle_antecedent_y['loin_bas']), consequent=movement_x_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=(obstacle_antecedent_x['proche_droite'], obstacle_antecedent_y['loin_haut']), consequent=movement_x_consequence['milieu']))
 
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_gauche'] & obstacle_antecedent['loin_droite']), consequent=movement_consequence['milieu']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_gauche'] & obstacle_antecedent['loin_droite']), consequent=movement_consequence['droite']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['proche_droite'] & obstacle_antecedent['loin_droite']), consequent=movement_consequence['gauche']))
-        rules.append(ctrl.Rule(antecedent=(wall_antecedent['loin_droite'] & obstacle_antecedent['loin_droite']), consequent=movement_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=obstacle_antecedent_y['loin_bas'], consequent=movement_y_consequence['milieu']))
+        rules.append(ctrl.Rule(antecedent=obstacle_antecedent_y['proche_bas'], consequent=movement_y_consequence['droite']))
+        rules.append(ctrl.Rule(antecedent=obstacle_antecedent_y['proche_haut'], consequent=movement_y_consequence['gauche']))
+        rules.append(ctrl.Rule(antecedent=obstacle_antecedent_y['loin_haut'], consequent=movement_y_consequence['milieu']))
 
         for rule in rules:
             rule.and_func = np.fmin
