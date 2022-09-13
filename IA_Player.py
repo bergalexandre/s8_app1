e46dca476be679a83222ac1055f7f0287d1fb9ca
Alexandre Bergeron <bera2920@usherbrooke.ca>
2022-09-13 17:03:16 -0400

    simplier fuzzy controller

--- IA_Player.py
+++ IA_Player.py
@@ -15,8 +15,7 @@ class IA_Player:
     def __init__(self, maze_tile_size):
         self.prolog_thread = self.mqi.create_thread()
         self.prolog_thread.query("[prolog/pathfinder]")
-        self.fuzzy_controller_left_right = IA_FuzzyController_left_right(maze_tile_size)
-        self.fuzzy_controller_up_down = IA_FuzzyController_down_up(maze_tile_size)
+        self.fuzzy_controller = IA_FuzzyController(maze_tile_size)
         self.maze_tile_size = maze_tile_size
 
     def __del__(self):
@@ -87,17 +86,28 @@ class IA_Player:
 
 
     def getClosestPerception(self, walls: list[pygame.Rect], obstacles: list[pygame.Rect], items: list[pygame.Rect], monsters, player):
-        longitude_wall = list(filter(lambda wall: self.isWallEast(wall, player) or self.isWallWest(wall, player), walls))
-        latitude_wall = list(filter(lambda wall: self.isWallNorth(wall, player) or self.isWallSouth(wall, player), walls))
+        east_walls = list(filter(lambda wall: self.isWallEast(wall, player), walls))
+        west_walls = list(filter(lambda wall: self.isWallWest(wall, player), walls))
+        north_walls = list(filter(lambda wall: self.isWallNorth(wall, player), walls))
+        south_walls = list(filter(lambda wall: self.isWallSouth(wall, player), walls))
 
-        distance_wall_x = PERCEPTION_RADIUS*self.maze_tile_size
-        distance_wall_y = PERCEPTION_RADIUS*self.maze_tile_size
+        distance_wall_x = [PERCEPTION_RADIUS*self.maze_tile_size]
+        distance_wall_y = [PERCEPTION_RADIUS*self.maze_tile_size]
 
-        if(len(longitude_wall) > 0):
-            distance_wall_x = min(np.array([wall.centerx for wall in longitude_wall]) - player.x, key=abs)
+        if len(east_walls) > 0:
+            distance_wall_x.append(np.array([(wall.centerx + wall.width )for wall in east_walls]) - player.x)
         
-        if(len(latitude_wall) > 0):
-            distance_wall_y = min(np.array([wall.centery for wall in latitude_wall]) - player.y, key=abs)
+        if len(west_walls) > 0:
+            distance_wall_x.append(player.x - np.array([(wall.centerx)for wall in east_walls]))
+        
+        if len(north_walls) > 0:
+            distance_wall_y.append(np.array([(wall.centery + wall.height )for wall in north_walls]) - player.y)
+        
+        if len(south_walls) > 0:
+            distance_wall_y.append(player.y - np.array([(wall.centery)for wall in south_walls]))
+
+        distance_wall_x = min(distance_wall_x, key=abs)
+        distance_wall_y = min(distance_wall_y, key=abs)
 
         if(len(obstacles) > 0):
             distance_obstacle_x = min(np.array([obstacle.centerx for obstacle in obstacles]) - player.x, key=abs)
@@ -120,31 +130,31 @@ class IA_Player:
         #distance_obstacle = rotation_matrice @ np.array([distance_wall_x, distance_wall_y])
          
 
-        force_x = self.fuzzy_controller_left_right.get_direction(distance_wall[0], distance_obstacle[0])
-        force_y = self.fuzzy_controller_up_down.get_direction(-distance_wall[1], -distance_obstacle[1])
+        force_x = self.fuzzy_controller.get_direction(distance_wall[0], distance_obstacle[0], -distance_obstacle[1])
+        force_y = self.fuzzy_controller.get_direction(-distance_wall[1], -distance_obstacle[1], distance_obstacle[0])
 
         #matrice de rotation
 
-        self.direction_courante = "south"
+        #self.direction_courante = "south"
         commandes = []
 
         if force_x > 0.2:
             commandes.append("RIGHT")
         elif force_x < -0.2:
             commandes.append("LEFT")
-        else:
-            if(self.direction_courante == "east"):
-                commandes.append("LEFT")
-            elif(self.direction_courante == "west"):
-                commandes.append("RIGHT")
+        #else:
+        #    if(self.direction_courante == "east"):
+        #        commandes.append("LEFT")
+        #    elif(self.direction_courante == "west"):
+        #        commandes.append("RIGHT")
         if force_y > 0.2:
             commandes.append("UP")
         elif force_y < -0.2:
             commandes.append("DOWN")
-        else:
-            if(self.direction_courante == "north"):
-                commandes.append("UP")
-            elif(self.direction_courante == "south"):
-                commandes.append("DOWN")
+        #else:
+        #    if(self.direction_courante == "north"):
+        #        commandes.append("UP")
+        #    elif(self.direction_courante == "south"):
+        #        commandes.append("DOWN")
 
         return commandes
