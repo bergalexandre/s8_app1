85712b6f74c5b6c5b0e819404de7ea23b4aa39be
Alexandre Bergeron <bera2920@usherbrooke.ca>
2022-09-14 21:15:02 -0400

    fix problème de virage et coinçage

--- IA_FuzzyController.py
+++ IA_FuzzyController.py
@@ -12,7 +12,7 @@ class IA_FuzzyController:
         perception_distance = maze_tile_size
         wall = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'wall')
         obstacle = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'obstacle')
-        obstacle_opp = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'obstacle_opp')
+        wall_obstacle = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'wall_obstacle')
         greedy_antecedent_x = ctrl.Antecedent(np.linspace(-perception_distance, perception_distance, 1000), 'greedy_x')
 
         movement = ctrl.Consequent(np.linspace(-1, 1, 1000), 'movement', defuzzify_method='centroid')
@@ -25,36 +25,37 @@ class IA_FuzzyController:
         wall["loin_gauche"] = fuzz.trapmf(wall.universe, loin_gauche)
         wall["moyen_gauche"] = fuzz.trapmf(wall.universe, moyen_gauche)
         wall["proche_gauche"] = fuzz.trapmf(wall.universe, proche_gauche)
-        wall["proche_droite"] = fuzz.trapmf(wall.universe, [abs(elem) for elem in reversed(proche_gauche)])
-        wall["moyen_droite"] = fuzz.trapmf(wall.universe, [abs(elem) for elem in reversed(moyen_gauche)])
-        wall["loin_droite"] = fuzz.trapmf(wall.universe, [abs(elem) for elem in reversed(loin_gauche)])
+        wall["proche_droite"] = fuzz.trapmf(wall.universe, [elem*-1 for elem in reversed(proche_gauche)])
+        wall["moyen_droite"] = fuzz.trapmf(wall.universe, [elem*-1 for elem in reversed(moyen_gauche)])
+        wall["loin_droite"] = fuzz.trapmf(wall.universe, [elem*-1 for elem in reversed(loin_gauche)])
+
+        loin_gauche = [-perception_distance, -perception_distance, -perception_distance*0.7, -perception_distance*0.4]
+        proche_gauche = [-perception_distance*0.6, -perception_distance*0.5, 0, 0]
+
+        wall_obstacle["loin_gauche"] = fuzz.trapmf(wall.universe, loin_gauche)
+        wall_obstacle["proche_gauche"] = fuzz.trapmf(wall.universe, proche_gauche)
+        wall_obstacle["proche_droite"] = fuzz.trapmf(wall.universe, [elem*-1 for elem in reversed(proche_gauche)])
+        wall_obstacle["loin_droite"] = fuzz.trapmf(wall.universe, [elem*-1 for elem in reversed(loin_gauche)])
 
         #loin_gauche = [-perception_distance, -perception_distance, -perception_distance*0.25, -perception_distance*0.2]
         #moyen_gauche = [-perception_distance*0.25, -perception_distance*0.2, -perception_distance*0.16, -perception_distance*0.15]
 
-        loin_gauche = [-perception_distance, -perception_distance, -perception_distance*0.35, -perception_distance*0.3]
-        proche_gauche = [-perception_distance*0.35, -perception_distance*0.25, 0, 0]
+        loin_gauche = [-perception_distance, -perception_distance, -perception_distance*0.35, -perception_distance*0.29]
+        proche_gauche = [-perception_distance*0.3, -perception_distance*0.25, 0, perception_distance*0.15]
         
         obstacle["loin_gauche"] = fuzz.trapmf(obstacle.universe, loin_gauche)
         #obstacle["moyen_gauche"] = fuzz.trapmf(obstacle.universe, moyen_gauche)
         obstacle["proche_gauche"] = fuzz.trapmf(obstacle.universe, proche_gauche)
-        obstacle["proche_droite"] = fuzz.trapmf(obstacle.universe, [abs(elem) for elem in reversed(proche_gauche)])
+        obstacle["proche_droite"] = fuzz.trapmf(obstacle.universe, [elem*-1 for elem in reversed(proche_gauche)])
         #obstacle["moyen_droite"] = fuzz.trapmf(obstacle.universe, [abs(elem) for elem in reversed(moyen_gauche)])
-        obstacle["loin_droite"] = fuzz.trapmf(obstacle.universe, [abs(elem) for elem in reversed(loin_gauche)])
-
-        loin_bas = [-perception_distance, -perception_distance, -perception_distance*0.35, -perception_distance*0.3]
-        proche_bas = [-perception_distance*0.35, -perception_distance*0.25, 0, 0]
-        obstacle_opp["loin_bas"] = fuzz.trapmf(obstacle_opp.universe, loin_bas)
-        obstacle_opp["proche_bas"] = fuzz.trapmf(obstacle_opp.universe, proche_bas)
-        obstacle_opp["proche_haut"] = fuzz.trapmf(obstacle_opp.universe, [abs(elem) for elem in reversed(loin_bas)])
-        obstacle_opp["loin_haut"] = fuzz.trapmf(obstacle_opp.universe, [abs(elem) for elem in reversed(proche_bas)])
+        obstacle["loin_droite"] = fuzz.trapmf(obstacle.universe, [elem*-1 for elem in reversed(loin_gauche)])
 
         movement["gauche"] = fuzz.trapmf(movement.universe, [-1, -1, -0.3 , -0.0])
         movement["milieu"] = fuzz.trimf(movement.universe, [-0.3,0,0.3])
         movement["droite"] = fuzz.trapmf(movement.universe, [0, 0.3, 1, 1])
 
         #pre-rule
-        obstacle_loin = (obstacle["loin_gauche"] | obstacle["loin_droite"])
+        obstacle_loin = (obstacle["loin_gauche"] | obstacle["loin_droite"]) & (wall_obstacle["loin_gauche"] | wall_obstacle["loin_droite"])
 
         #rules
         rules = []
@@ -68,7 +69,11 @@ class IA_FuzzyController:
         rules.append(ctrl.Rule(antecedent=(wall["moyen_gauche"] & obstacle_loin), consequent=movement["droite"]))
         rules.append(ctrl.Rule(antecedent=(wall["moyen_droite"] & obstacle_loin), consequent=movement["gauche"]))
 
+        rules.append(ctrl.Rule(antecedent=(obstacle["proche_droite"] & obstacle["proche_gauche"]), consequent=movement["droite"])) # (ノ>_<)ノ
 
+        rules.append(ctrl.Rule(antecedent=(wall_obstacle["proche_droite"]), consequent=movement["gauche"]))
+        rules.append(ctrl.Rule(antecedent=(wall_obstacle["proche_gauche"]), consequent=movement["droite"]))
+        
         rules.append(ctrl.Rule(antecedent=((wall["loin_gauche"] | wall["moyen_gauche"]) & obstacle["proche_gauche"]), consequent=movement["droite"]))
         rules.append(ctrl.Rule(antecedent=((wall["loin_droite"] | wall["moyen_droite"]) & obstacle["proche_droite"]), consequent=movement["gauche"]))
         rules.append(ctrl.Rule(antecedent=((wall["loin_gauche"] | wall["moyen_gauche"]) & obstacle["proche_droite"]), consequent=movement["gauche"]))
@@ -94,9 +99,10 @@ class IA_FuzzyController:
             var.view()
         plt.show()
 
-    def get_direction(self, wall, obstacle):
+    def get_direction(self, wall, obstacle, wall_obstacle):
         self.sim.input["wall"] = wall
         self.sim.input["obstacle"] = obstacle
+        self.sim.input["wall_obstacle"] = wall_obstacle
         #self.sim.input["obstacle_opp"] = obstacle_opp
 
         self.sim.compute()
